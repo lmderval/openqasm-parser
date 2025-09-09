3e829cbc47cf256e57728683f6a041ae726d2cf8
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-09-09 21:38:12 +0200

    chore(factory): implement a factory to build AST nodes

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,2 +1,3 @@
+pub mod factory;
 pub mod node;
 pub mod visitor;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use crate::ast::{factory, node};
 
 pub mod ast;
 pub mod chars;
@@ -7,13 +7,45 @@ pub mod location;
 pub mod token;
 
 fn main() {
-    let mut lexer = lexer::build_lexer(String::from("<stdin>"), io::stdin());
-    while let Some(token) = lexer.peek() {
-        println!("{}", token.to_string());
-        match token.get_ty() {
-            token::TokenTy::Eof => break,
-            _ => {}
-        }
-        lexer.drop();
-    }
+    let loc = location::build_location(String::from("<stdin>"), 0, 0, 0, 0);
+    let program = vec![
+        factory::make_reg_dec(loc.clone(), String::from("qr0"), node::RegTy::QReg, 2),
+        factory::make_reg_dec(loc.clone(), String::from("qr1"), node::RegTy::QReg, 2),
+        factory::make_reg_dec(loc.clone(), String::from("cr0"), node::RegTy::CReg, 2),
+        factory::make_reg_dec(loc.clone(), String::from("cr1"), node::RegTy::CReg, 2),
+        factory::make_gate_stmt(
+            loc.clone(),
+            String::from("U"),
+            vec![
+                factory::make_binop_exp(
+                    loc.clone(),
+                    factory::make_pi_exp(loc.clone()),
+                    node::BinopTy::Div,
+                    factory::make_int_exp(loc.clone(), 2),
+                ),
+                factory::make_int_exp(loc.clone(), 0),
+                factory::make_pi_exp(loc.clone()),
+            ],
+            vec![factory::make_simple_reg(loc.clone(), String::from("qr0"))],
+        ),
+        factory::make_gate_stmt(
+            loc.clone(),
+            String::from("CX"),
+            vec![],
+            vec![
+                factory::make_subscript_reg(loc.clone(), String::from("qr0"), 0),
+                factory::make_subscript_reg(loc.clone(), String::from("qr1"), 0),
+            ],
+        ),
+        factory::make_measure_stmt(
+            loc.clone(),
+            factory::make_simple_reg(loc.clone(), String::from("qr0")),
+            factory::make_simple_reg(loc.clone(), String::from("cr0")),
+        ),
+        factory::make_measure_stmt(
+            loc.clone(),
+            factory::make_simple_reg(loc.clone(), String::from("qr1")),
+            factory::make_simple_reg(loc.clone(), String::from("cr1")),
+        ),
+    ];
 }
