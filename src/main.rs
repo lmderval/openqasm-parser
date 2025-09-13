9674c819d7cdbd94bbe8df5ce033c1ab465aefdc
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-09-13 11:41:11 +0200

    feat: read and parse an OpenQASM algorithm and pretty print it

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ pub mod ast;
 pub mod parse;
 pub mod utils;
 
+use crate::ast::pretty_printer::PrettyPrinter;
+
+use crate::parse::lexer::Lexer;
+use crate::parse::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let lexer = Lexer::new(String::from("<stdin>"), std::io::stdin());
+    let mut parser = Parser::new(lexer);
+    let program = parser.parse_input();
+    program.iter().for_each(|it| PrettyPrinter.pretty_print(it));
+    parser
+        .get_error()
+        .iter()
+        .for_each(|it| println!("{}", it.to_string()));
 }
