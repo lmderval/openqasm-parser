da055265c314192cfc0e6c269206565f5b8b535c
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-09-13 11:15:42 +0200

    chore(location): create helper to handle location in a file

---
+++ src/utils/location.rs
@@ -0,0 +1,93 @@
+pub struct Location {
+    file: String,
+    line_start: u64,
+    column_start: u64,
+    line_end: u64,
+    column_end: u64,
+}
+
+impl ToString for Location {
+    fn to_string(&self) -> String {
+        if self.line_start == self.line_end {
+            if self.column_start == self.column_end {
+                self.file.clone()
+                    + ":"
+                    + &self.line_start.to_string()
+                    + ":"
+                    + &self.column_start.to_string()
+            } else {
+                self.file.clone()
+                    + ":"
+                    + &self.line_start.to_string()
+                    + ":"
+                    + &self.column_start.to_string()
+                    + "-"
+                    + &self.column_end.to_string()
+            }
+        } else {
+            self.file.clone()
+                + ":"
+                + &self.line_start.to_string()
+                + ":"
+                + &self.column_start.to_string()
+                + "-"
+                + &self.line_end.to_string()
+                + ":"
+                + &self.column_end.to_string()
+        }
+    }
+}
+
+impl Clone for Location {
+    fn clone(&self) -> Location {
+        Location {
+            file: self.file.clone(),
+            line_start: self.line_start,
+            column_start: self.column_start,
+            line_end: self.line_end,
+            column_end: self.column_end,
+        }
+    }
+}
+
+impl Location {
+    pub fn new(
+        file: String,
+        line_start: u64,
+        column_start: u64,
+        line_end: u64,
+        column_end: u64,
+    ) -> Location {
+        Location {
+            file: file,
+            line_start: line_start,
+            column_start: column_start,
+            line_end: line_end,
+            column_end: column_end,
+        }
+    }
+
+    pub fn start_to_end(&mut self) {
+        self.line_start = self.line_end;
+        self.column_start = self.column_end;
+    }
+
+    pub fn step_line(&mut self) {
+        self.line_end += 1;
+        self.column_end = 0;
+    }
+
+    pub fn step_column(&mut self) {
+        self.column_end += 1;
+    }
+
+    pub fn end_to_start(&mut self, next: &Location) {
+        self.line_end = next.line_start;
+        self.column_end = next.column_start;
+    }
+
+    pub fn end_to_end(&mut self, next: &Location) {
+        self.line_end = next.line_end;
+        self.column_end = next.column_end;
+    }
+}
