8d8221ea4b56d78c554f55733970ccaa201e1e32
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-09-15 00:38:21 +0200

    feat: bind the previously parsed program

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,27 @@ pub mod utils;
 
 use crate::ast::pretty_printer::PrettyPrinter;
 
+use crate::bind::binder::Binder;
+
 use crate::parse::lexer::Lexer;
 use crate::parse::parser::Parser;
 
 fn main() {
     let lexer = Lexer::new(String::from("<stdin>"), std::io::stdin());
     let mut parser = Parser::new(lexer);
-    let program = parser.parse_input();
-    program.iter().for_each(|it| PrettyPrinter.pretty_print(it));
+    let mut binder = Binder::new();
+
+    if let Some(program) = &mut parser.parse_input() {
+        binder.bind(program);
+
+        PrettyPrinter.pretty_print(program);
+    }
+
     parser
+        .get_error()
+        .iter()
+        .for_each(|it| println!("{}", it.to_string()));
+    binder
         .get_error()
         .iter()
         .for_each(|it| println!("{}", it.to_string()));
