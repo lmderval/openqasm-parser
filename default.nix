993abff475fd26bcd4e7782a373bef0d72f99108
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-09-18 21:28:34 +0200

    chore(version): bump version to 0.2.0

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 let
   openqasm-parser = pkgs.rustPlatform.buildRustPackage {
     pname = "openqasm-parser";
-    version = "0.1.0";
+    version = "0.2.0";
     src = localLib.filters.cleanSourceWithFiles {
       src = ./.;
       files = [
